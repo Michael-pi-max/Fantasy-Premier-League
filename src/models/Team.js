f5fda893ff6c96ce35982617269529b8adf28609
AbenezerAtnafu <se.abenezer.atnafu@gmail.com>
2021-02-21 16:18:06 +0300

    emeblem added from fantasy web

--- src/models/Team.js
+++ src/models/Team.js
@@ -1,3 +1,40 @@
+let emblem = {
+  Arsenal: "https://resources.premierleague.com/premierleague/badges/50/t3.png",
+  "Aston Villa":
+    "https://resources.premierleague.com/premierleague/badges/50/t7.png",
+  Brighton:
+    "https://resources.premierleague.com/premierleague/badges/50/t36.png",
+  Burnley:
+    "https://resources.premierleague.com/premierleague/badges/50/t90.png",
+  Chelsea: "https://resources.premierleague.com/premierleague/badges/50/t8.png",
+  "Crystal Palace":
+    "https://resources.premierleague.com/premierleague/badges/50/t31.png",
+  Everton:
+    "https://resources.premierleague.com/premierleague/badges/50/t11.png",
+  Fulham: "https://resources.premierleague.com/premierleague/badges/50/t54.png",
+  Leeds: "https://resources.premierleague.com/premierleague/badges/50/t2.png",
+  Leicester:
+    "https://resources.premierleague.com/premierleague/badges/50/t13.png",
+  Liverpool:
+    "https://resources.premierleague.com/premierleague/badges/50/t14.png",
+  "Man City":
+    "https://resources.premierleague.com/premierleague/badges/50/t43.png",
+  "Man Utd":
+    "https://resources.premierleague.com/premierleague/badges/50/t1.png",
+  Newcastle:
+    "https://resources.premierleague.com/premierleague/badges/50/t4.png",
+  "Sheffield Utd":
+    "https://resources.premierleague.com/premierleague/badges/50/t49.png",
+  Southampton:
+    "https://resources.premierleague.com/premierleague/badges/50/t20.png",
+  Spurs: "https://resources.premierleague.com/premierleague/badges/50/t6.png",
+  "West Brom":
+    "https://resources.premierleague.com/premierleague/badges/50/t35.png",
+  "West Ham":
+    "https://resources.premierleague.com/premierleague/badges/50/t21.png",
+  Wolves: "https://resources.premierleague.com/premierleague/badges/50/t39.png",
+};
+
 let arena = {
   Arsenal: "Emirates Stadium",
   "Aston Villa": "Villa Park",
