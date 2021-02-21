bfea80bbd0f2e86e78a010b81b834c21bf02ecf6
AbenezerAtnafu <se.abenezer.atnafu@gmail.com>
2021-02-21 16:17:02 +0300

    jersy added from url

--- src/models/Team.js
+++ src/models/Team.js
@@ -58,27 +58,47 @@ let arena = {
   Wolves: "Molineux Stadium",
 };
 
-let arena2 = {
-  Arsenal: "Emirates Stadium",
-  "Aston Villa": "Villa Park",
-  Brighton: "American Express",
-  Burnley: "Turf Moor",
-  Chelsea: "Stamford Bridge",
-  "Crystal Palace": "Selhurst Park",
-  Everton: "Goodison Park",
-  Fulham: "Craven Cottage",
-  Leeds: "Elland Road",
-  Leicester: "King Power",
-  Liverpool: "Anfield",
-  "Man City": "Eithad",
-  "Man Utd": "Old Trafford",
-  Newcastle: "St. James' Park",
-  "Sheffield Utd": "Bramall Lane",
-  Southampton: "St Mary",
-  Spurs: "Tottenham Hotspur",
-  "West Brom": "Albion Park",
-  "West Ham": "London Stadium",
-  Wolves: "Molineux Stadium",
+let jersey = {
+  Arsenal:
+    "https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_3-66.webp",
+  "Aston Villa":
+    "https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_7-66.webp",
+  Brighton:
+    "https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_36-66.webp",
+  Burnley:
+    "https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_90-66.webp",
+  Chelsea:
+    "https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_8-66.webp",
+  "Crystal Palace":
+    "https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_31-66.webp",
+  Everton:
+    "https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_11-66.webp",
+  Fulham:
+    "https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_54-66.webp",
+  Leeds:
+    "https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_2-66.webp",
+  Leicester:
+    "https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_13-66.webp",
+  Liverpool:
+    "https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_14-66.webp",
+  "Man City":
+    "https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_43-66.webp",
+  "Man Utd":
+    "https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_1-66.webp",
+  Newcastle:
+    "https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_4-66.webp",
+  "Sheffield Utd":
+    "https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_49-66.webp",
+  Southampton:
+    "https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_20-66.webp",
+  Spurs:
+    "https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_6-66.webp",
+  "West Brom":
+    "https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_35-66.webp",
+  "West Ham":
+    "https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_21-66.webp",
+  Wolves:
+    "https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_39-66.webp",
 };
 
 let jersey = {
